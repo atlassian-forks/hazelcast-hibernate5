f27de36efc47b9e62dde0a906ff96ed0e69cf135
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-04-10 08:15:51 +0200

    Fix eviction algorithm

--- hazelcast-hibernate5/src/main/java/com/hazelcast/hibernate/local/LocalRegionCache.java
+++ hazelcast-hibernate5/src/main/java/com/hazelcast/hibernate/local/LocalRegionCache.java
@@ -308,7 +308,6 @@ public class LocalRegionCache implements RegionCache {
         }
     }
 
-
     protected void maybeInvalidate(final Object messageObject) {
         Invalidation invalidation = (Invalidation) messageObject;
         Object key = invalidation.getKey();
